c52a92109b2ab044155b2f442d35a2ed5c646f8c
Niels Swimberghe <nielsswimberghe@gmail.com>
2019-07-20 22:48:33 -0400

    Retarget to .NET Core 2.2 + use runsettings in tests

--- Demo.SeleniumTests/DotNetSiteTests.cs
+++ Demo.SeleniumTests/DotNetSiteTests.cs
@@ -8,24 +8,28 @@ namespace Demo.SeleniumTests
     [TestClass]
     public class DotNetSiteTests
     {
+        public TestContext TestContext { get; set; }
+
         [TestMethod]
         public void TestGetStarted()
         {
-            //Chrome Driver was manually downloaded from https://sites.google.com/a/chromium.org/chromedriver/downloads
-            using (var driver = new ChromeDriver()){
-
+            // Chrome Driver was manually downloaded from https://sites.google.com/a/chromium.org/chromedriver/downloads
+            // parameter "." will instruct to look for the chromedriver.exe in the current folder (bin/debug/...)
+            using (var driver = GetDriver())
+            {
                 //Navigate to DotNet website
-                driver.Navigate().GoToUrl("https://dotnet.microsoft.com/");
+                driver.Navigate().GoToUrl((string)TestContext.Properties["webAppUrl"]);
                 //Click the Get Started button
                 driver.FindElement(By.LinkText("Get Started")).Click();
 
                 // Get Started section is a multi-step wizard
                 // The following sections will find the visible next step button until there's no next step button left
                 IWebElement nextLink = null;
-                do{
+                do
+                {
                     nextLink?.Click();
                     nextLink = driver.FindElements(By.CssSelector(".step:not([style='display:none;']):not([style='display: none;']) .step-select")).FirstOrDefault();
-                }while(nextLink != null);
+                } while (nextLink != null);
 
                 // on the last step, grab the title of the step wich should be equal to "Next steps"
                 var lastStepTitle = driver.FindElement(By.CssSelector(".step:not([style='display:none;']):not([style='display: none;']) h2")).Text;
@@ -33,6 +37,18 @@ namespace Demo.SeleniumTests
                 // verify the title is the expected value "Next steps"
                 Assert.AreEqual(lastStepTitle, "Next steps");
             }
+        }
+
+        private ChromeDriver GetDriver()
+        {
+            var options = new ChromeOptions();
+
+            if(bool.Parse((string)TestContext.Properties["headless"]))
+            {
+                options.AddArgument("headless");
+            }
+
+            return new ChromeDriver(".", options);
         }
     }
 }
